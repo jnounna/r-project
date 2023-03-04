aa8e5dbe999cc7c503dc489c5e8f26182381f267
jnounna <97972572+jnounna@users.noreply.github.com>
2023-03-04 11:36:14 -0700

    first commit

---
+++ mtrend.r
@@ -0,0 +1,13 @@
+require(graphics)
+pairs(mtcars, main = "mtcars data", gap = 1/4)
+coplot(mpg ~ disp | as.factor(cyl), data = mtcars,
+       panel = panel.smooth, rows = 1)
+
+mtcars2 <- within(mtcars, {
+   vs <- factor(vs, labels = c("V", "S"))
+   am <- factor(am, labels = c("automatic", "manual"))
+   cyl  <- ordered(cyl)
+   gear <- ordered(gear)
+   carb <- ordered(carb)
+})
+summary(mtcars2)
